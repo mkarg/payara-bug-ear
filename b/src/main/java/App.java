1860aa1e515e31ae46ed5aa311d9557f3f08bcb8
Markus KARG <karg@quipsy.de>
2019-02-14 17:47:16 +0100

    AppClient fails with 'Cannot find scanner for car'

--- b/src/main/java/App.java
+++
@@ -1,4 +0,0 @@
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("") public class App extends Application { }
--- b/src/main/java/Resource.java
+++
@@ -1,8 +0,0 @@
-import javax.ws.rs.Path;
-import javax.ws.rs.GET;
-
-@Path("") public class Resource {
-
-  @GET public void getResponse() { }
-
-}
